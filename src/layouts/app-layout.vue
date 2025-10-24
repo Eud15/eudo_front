adfcd5a5f2e60b45309761ee6c80af504072e6e7
Eudoxie Abouta <aboutaeudoxie@gmail.com>
2025-10-24 12:54:37 +0000

    Eudoxie

--- src/layouts/app-layout.vue
+++ src/layouts/app-layout.vue
@@ -9,7 +9,7 @@
             v-show="store.isShowMainLoader"
             class="screen_loader fixed inset-0 bg-[#fafafa] dark:bg-[#060818] z-[60] grid place-content-center animate__animated"
         >
-            <svg width="64" height="64" viewBox="0 0 135 135" xmlns="http://www.w3.org/2000/svg" fill="#4361ee">
+            <svg width="64" height="64" viewBox="0 0 135 135" xmlns="http://www.w3.org/2000/svg" fill="#ED469B" class="text-pink">
                 <path
                     d="M67.447 58c5.523 0 10-4.477 10-10s-4.477-10-10-10-10 4.477-10 10 4.477 10 10 10zm9.448 9.447c0 5.523 4.477 10 10 10 5.522 0 10-4.477 10-10s-4.478-10-10-10c-5.523 0-10 4.477-10 10zm-9.448 9.448c-5.523 0-10 4.477-10 10 0 5.522 4.477 10 10 10s10-4.478 10-10c0-5.523-4.477-10-10-10zM58 67.447c0-5.523-4.477-10-10-10s-10 4.477-10 10 4.477 10 10 10 10-4.477 10-10z"
                 >
@@ -23,7 +23,7 @@
             </svg>
         </div>
 
-        <div class="fixed bottom-6 ltr:right-6 rtl:left-6 z-50">
+        <!-- <div class="fixed bottom-6 ltr:right-6 rtl:left-6 z-50">
             <template v-if="showTopButton">
                 <button
                     type="button"
@@ -45,7 +45,7 @@
                     </svg>
                 </button>
             </template>
-        </div>
+        </div> -->
 
         <!-- BEGIN APP SETTING LAUNCHER -->
         <Setting />
