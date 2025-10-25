aab47c63c9ecfe35f24e7133d145f2833ca8df76
primo280 <dohaprimael06gmail.com>
2025-10-25 01:05:07 +0100

    fix content type

--- api/proxy.js
+++ api/proxy.js
@@ -1,25 +1,12 @@
 export default async function handler(req, res) {
-  // Autorisations du  CORS (important si ton frontend appelle ce proxy)
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-
-  if (req.method === 'OPTIONS') {
-    return res.status(200).end();
-  }
-
-  // URL complète vers ton backend
-  const backendUrl = `http://31.97.53.56:8050${req.url}`;
+  const backendUrl = `http://31.97.53.56:8050${req.url}`; 
 
   try {
-    // Filtrer certains headers incompatibles
-    const { host, connection, 'content-length': _, ...safeHeaders } = req.headers;
-
     const response = await fetch(backendUrl, {
       method: req.method,
       headers: {
         'Content-Type': 'application/json',
-        ...safeHeaders,
+        ...req.headers, 
       },
       body: req.method !== 'GET' ? JSON.stringify(req.body) : undefined,
     });
